13c77ce891e03ad2bc6e1683b472b5595123bf80
Yan Klyuzhin <klyuzhin@uchi.ru>
2023-08-21 07:10:09 +0300

    added font & grouped some common styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import './fonts/Roboto-Regular.ttf';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
